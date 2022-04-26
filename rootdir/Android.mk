8f7fb09f022690e27c6bc6e9e56557748927a28d
SahilSonar <sss.sonar2003@gmail.com>
2022-04-26 14:15:49 +0200

    sm8150-common: rootdir: Move init.recovery.qcom.rc to Android.bp
    Change-Id: I452b4f3c5d43c54d477833382e3257be6a4617fa
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,13 +17,3 @@ endif
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 endif
 include $(BUILD_PREBUILT)
-
-# prebuilt_root doesn't cover recovery ramdisk, that sucks
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.qcom.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
