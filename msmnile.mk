f11bf6ef38ca484c1fe55b91d6bb61d7f8b6c2bc
alanndz <alanndz7@gmail.com>
2022-08-20 03:28:08 +0900

    sm8150-common: Add soong config namespace for thermal
    * Reference https://android.googlesource.com/platform/hardware/google/pixel/+/refs/tags/android-13.0.0_r1/thermal/device.mk
    
    Signed-off-by: alanndz <alanndz7@gmail.com>
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -487,6 +487,12 @@ PRODUCT_BOOT_JARS += \
     xiaomi-telephony-stub
 
 # Thermal HAL
+SOONG_CONFIG_NAMESPACES += thermal_hal_feature
+SOONG_CONFIG_thermal_hal_feature += \
+    pid \
+
+SOONG_CONFIG_thermal_hal_feature_pid ?= apply_1_0
+
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.pixel \
     thermal_symlinks
