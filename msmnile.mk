5780c2a52d8d3198923aa00a91867bd04b082fb2
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    sm8150-common: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -491,7 +491,9 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
