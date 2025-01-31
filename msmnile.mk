98f7f28a517ef1cb9d806cf3a31102f2bd86e129
LuK1337 <priv.luk@gmail.com>
2025-01-31 10:28:03 +0100

    sm8150-common: Move media_codecs_c2.xml to device tree
    neobuddy89: Drop media_codecs_vendor_audio.xml while at it.
    
    Change-Id: Ib2ebfff4281eafd46427180c2666a71d0d144300
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -329,6 +329,7 @@ PRODUCT_PACKAGES += \
     libstagefright_foundation-v33
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
 
 # Mlipay
