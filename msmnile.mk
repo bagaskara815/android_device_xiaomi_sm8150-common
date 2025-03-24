73d4e1efce9934d1b0c6834b02347adadc32d905
Pranav Vashi <neobuddy89@gmail.com>
2025-03-24 21:55:56 +0530

    Revert "sm8150-common: Explicitly set as an non-AB target"
    This reverts commit 289305503a4095933abd38b0987aa5a6d9d92cc8.
    
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -484,9 +484,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
-# Update
-AB_OTA_UPDATER := false
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
