37ace3dcf8c6e809c52f3ecdc67e64d237514ad9
Pranav Vashi <neobuddy89@gmail.com>
2025-04-04 12:02:39 +0530

    sm8150-common: Build DeviceAsWebcam service
    * We disabled this by default on crdroid.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -156,6 +156,8 @@ PRODUCT_PACKAGES += \
 TARGET_ENABLE_BLUR := true
 
 # Camera
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
 PRODUCT_PACKAGES += \
