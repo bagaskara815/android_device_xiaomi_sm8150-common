LineageOS/android_device_oneplus_sm8150-common

sm8150-common: Shim widevine with libcrypto_shim
The following change
https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
removed symbols that our libs require.

Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

extract-files.sh
@@ -76,6 +76,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -ni "/android.hardware.biometrics.fingerprint/{x;s/hal format/hal override=\"true\" format/;x};x;1!p;\${x;p}" "${2}"
             ;;
+        odm/lib64/libwvhidl.so|odm/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         product/app/PowerOffAlarm/PowerOffAlarm.apk)
             [ "$2" = "" ] && return 0
             apktool_patch "${2}" "${MY_DIR}/blob-patches/PowerOffAlarm.patch" -s

bagaskara815/android_device_xiaomi_sm8150-common

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

extract-files.sh
@@ -76,6 +76,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             fix_xml "${2}"
             ;;
+        vendor/lib64/libwvhidl.so | vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;